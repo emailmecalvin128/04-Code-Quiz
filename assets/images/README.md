58606af29d3402030ff95e1b354c4b4063ec04fc
Calvin Lee <emailmecalvin@gmail.com>
2020-10-26 12:08:12 -0400

    redo

---
+++ assets/images/README.md
@@ -0,0 +1,4 @@
+# Week 4 Quiz 
+
+# Porsche Quiz 
+* multiple choice questions about the car company porsche. A time code quiz with multiple-choice questions. This app will run in the browser and feature dynamic html, css and js. 
---
+++ question.js
@@ -0,0 +1,45 @@
+
+var questions = [
+  {
+    question:
+      "What year did porsche produce its first electric vehicle'?",
+    optionA: "A. 2019",
+    optionB: "B. 1899",
+    optionC: "C. 1999",
+    optionD: "D. 1965",
+    answer: "B",
+  },
+  {
+    question: "Where was the very first beetle VW built?",
+    optionA: "A. Porsche Headquarters",
+    optionB: "B. Alambama",
+    optionC: "C. Ferdinands private Villa",
+    optionD: "D. England,London",
+    answer: "C",
+  },
+  {
+    question: "How many different production models of 911 are there?",
+    optionA: "A. 4",
+    optionB: "B. 31",
+    optionC: "C. 12",
+    optionD: "D. 16",
+    answer: "D",
+  },
+  {
+    question:
+      "The very first road sports car Porsche had what kind of engine?",
+    optionA: "A. rear-engine",
+    optionB: "B. mid-engine",
+    optionC: "C. front-engine",
+    optionD: "D. rotary-engine",
+    answer: "B",
+  },
+  {
+    question: "What other vehicles has porsche created?",
+    optionA: "A. airplanes",
+    optionB: "B. motorboats",
+    optionC: "C. trains",
+    optionD: "D. farm-tractors",
+    answer: "D",
+  },
+];
